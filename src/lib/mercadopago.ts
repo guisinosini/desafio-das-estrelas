bf76a780409780739ccf3f30b339eade512ed9d4
Seu Nome ou Instituto Kamaleon <guisinosini@gmail.com>
2026-05-19 19:01:34 -0300

    ok36

--- src/lib/mercadopago.ts
+++ src/lib/mercadopago.ts
@@ -1,10 +1,6 @@
 import { MercadoPagoConfig } from 'mercadopago';
 
-if (!process.env.MERCADOPAGO_ACCESS_TOKEN) {
-  throw new Error('MERCADOPAGO_ACCESS_TOKEN is missing in environment variables.');
-}
-
 export const mpClient = new MercadoPagoConfig({
-  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,
+  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN || '',
   options: { timeout: 5000 }
 });
